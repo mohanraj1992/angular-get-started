d758fdab8cf6c78cec3aaa451925a9e2c50d490a
mohanraj1992 <kctcsmohan@gmail.com>
2017-10-05 18:43:04 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PeopleListComponent } from './people-list/people-list.component';
+import { PersonDetailsComponent } from './person-details/person-details.component';
+import { AppRoutingModule } from "./app.routing.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PeopleListComponent,
+    PersonDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/people-list/people-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Person } from '../person';
+import {PeopleService} from '../people.service';
+
+@Component({
+  selector: 'app-people-list',
+  template: `
+    <p>
+      people-list Works!
+    </p>
+    <ul>
+      <li *ngFor="let person of people">
+        <a [routerLink]="['/persons', person.id]">
+        {{person.name}}
+        </a>
+      </li>
+    </ul>
+    <app-person-details></app-person-details>
+
+  `,
+  styleUrls: ['./people-list.component.scss']
+})
+export class PeopleListComponent implements OnInit {
+
+  people: Person[] = [];
+
+
+  constructor(private _peopleService: PeopleService) { }
+
+
+
+  ngOnInit() {
+    this.people = this._peopleService.getAll();
+  }
+
+}
